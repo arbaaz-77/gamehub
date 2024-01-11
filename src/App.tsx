92a57aa83b30163fe0f84bbae4ba4a3e1cfec6ba
Arbaaz <arbaaz970@gmail.com>
2024-01-11 11:47:32 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem
+        area={"nav"}
+        bg="coral"
+      >
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem
+          area={"aside"}
+          bg="gold"
+        >
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem
+        area={"main"}
+        bg="dodgerBlue"
+      >
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
